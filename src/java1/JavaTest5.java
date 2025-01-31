453870e3e11dd68095c5ba43abf46f9499dd5887
klemus8 <klemush8@gmail.com>
2025-01-30 18:33:26 -0600

    Update JavaTest5.java

--- src/java1/JavaTest5.java
+++ src/java1/JavaTest5.java
@@ -3,6 +3,7 @@ package java1;
 public class JavaTest5 {
 	public static void main(String[] args) {
 		System.out.println("GitHub testing homework");
+		System.out.println("Learning to use Pulling from GitHub");
 	}
 
 }
