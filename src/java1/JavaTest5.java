f0220d30bd8d137c8cf9b33321097340d5101069
karla <karla@192.168.1.154>
2025-01-30 18:32:17 -0600

    Updated String

--- src/java1/JavaTest5.java
+++ src/java1/JavaTest5.java
@@ -2,7 +2,7 @@ package java1;
 
 public class JavaTest5 {
 	public static void main(String[] args) {
-		System.out.println("Java testing homework");
+		System.out.println("GitHub testing homework");
 	}
 
 }
